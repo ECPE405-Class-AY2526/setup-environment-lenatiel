e2554d834e26ae3793a57b4f44f317bbd8982447
lenatiel <meggomez03@gmail.com>
2025-08-07 15:03:23 +0800

    FINAL

---
+++ client/landingpage/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function LandingPage() {
+  return (
+    <div>
+      <h1>Mask Detection App</h1>
+      <p>Welcome to the client-side interface.</p>
+    </div>
+  );
+}
+
+export default LandingPage;
